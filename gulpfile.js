d747d370f8d006f075599b9891a1cb91ab3cc8cb
Unknown <alexeygedz@gmail.com>
2019-03-11 18:54:38 +0500

    gulp настройка сборщика

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
 const gulp =  require('gulp');
 const less = require('gulp-less');
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const server = require("browser-sync").create();
 
-gulp.task('less', function () {
-   return gulp.src('source/less/style.less')
+gulp.task("style", function () {
+   return gulp.src("source/less/style.less")
+       .pipe(plumber())
        .pipe(less())
-       .pipe(gulp.dest('source/css'));
-
+       .pipe(postcss([
+           autoprefixer()
+       ]))
+       .pipe(gulp.dest("source/css"))
+       .pipe(server.stream());
 });
 
-gulp.task('watch', function () {
-   gulp.watch('source/less/style.less', gulp.parallel('less'));
+gulp.task("serve", ["style"], function () {
+    server.init({
+        server: "source/"
+    });
+
+    gulp.watch("source/less/**/*.less", ["style"]);
+    gulp.watch("source/*.html")
+        .on("change", server.reload);
 });
 
-gulp.task('default', gulp.parallel('less'));
+// gulp.task('watch', function () {
+//    gulp.watch('source/less/style.less', gulp.parallel('less'));
+// });
+//
+// gulp.task('default', gulp.parallel('less'));
