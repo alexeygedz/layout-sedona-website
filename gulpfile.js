87d833197249ed46620ad25435892a83fd29b3e0
Unknown <alexeygedz@gmail.com>
2019-03-12 16:14:16 +0500

    недоделки

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,42 @@
-const gulp =  require('gulp');
-const less = require('gulp-less');
-const plumber = require("gulp-plumber");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const server = require("browser-sync").create();
-
-gulp.task("style", function () {
-   return gulp.src("source/less/style.less")
-       .pipe(plumber())
-       .pipe(less())
-       .pipe(postcss([
-           autoprefixer()
-       ]))
-       .pipe(gulp.dest("source/css"))
-       .pipe(server.stream());
-});
-
-gulp.task("serve", ["style"], function () {
-    server.init({
-        server: "source/"
-    });
-
-    gulp.watch("source/less/**/*.less", ["style"]);
-    gulp.watch("source/*.html")
-        .on("change", server.reload);
-});
-
-// gulp.task('watch', function () {
-//    gulp.watch('source/less/style.less', gulp.parallel('less'));
+// const gulp =  require('gulp');
+// const less = require('gulp-less');
+//
+// gulp.task('less', function () {
+//     return gulp.src('source/less/style.less')
+//         .pipe(less())
+//         .pipe(gulp.dest('source/css'));
+//
 // });
 //
-// gulp.task('default', gulp.parallel('less'));
+// gulp.task('watch', function () {
+//     gulp.watch('source/less/style.less', gulp.parallel('less'));
+// });
+//
+// gulp.task('default', gulp.parallel('less', 'watch'));
+
+var gulp        = require('gulp'),
+    less        = require('gulp-less'),
+    browserSync = require('browser-sync');
+
+gulp.task('less', function() {
+    return gulp.src('source/less/**/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('source/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'source'
+        },
+        port: 63342,
+        notify: false,
+    });
+    browserSync.reload();
+});
+
+
+gulp.task('watch', gulp.parallel('browser-sync', 'less', function() {
+    gulp.watch('source/less/**/*.less', gulp.parallel('less'));
+}));
